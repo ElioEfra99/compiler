84e5a0f739fbb7c15a481fcef340fefdfffca58f
Eliú Díaz <eedb_abcd_1234567890@ymail.com>
2020-04-10 11:56:25 -0500

    Initial commit

---
+++ BhWin32/Resource.h
@@ -0,0 +1,36 @@
+//{{NO_DEPENDENCIES}}
+// Archivo de inclusión generado de Microsoft Visual C++.
+// Usado por BhWin32.rc
+
+#define IDS_APP_TITLE			103
+
+#define IDR_MAINFRAME			128
+#define IDD_BHWIN32_DIALOG	102
+#define IDD_ABOUTBOX			103
+#define IDM_ABOUT				104
+#define IDM_EXIT				105
+#define IDI_BHWIN32			107
+#define IDI_SMALL				108
+#define IDC_BHWIN32			109
+#define IDC_MYICON				2
+#ifndef IDC_STATIC
+#define IDC_STATIC				-1
+// Valores predeterminados siguientes para nuevos objetos
+//
+#define ID_BTNINICIO 201
+#define ID_BTNCOMPILAR 202
+#define IDB_TOOLBAR 300
+#endif
+// Valores predeterminados siguientes para nuevos objetos
+//
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+
+#define _APS_NO_MFC					130
+#define _APS_NEXT_RESOURCE_VALUE	129
+#define _APS_NEXT_COMMAND_VALUE		32771
+#define _APS_NEXT_CONTROL_VALUE		1000
+#define _APS_NEXT_SYMED_VALUE		110
+
+#endif
+#endif
